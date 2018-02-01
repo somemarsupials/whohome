58cf85cbfaf9ca1303187b41ea598784c73358f3
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-02-01 16:21:13 +0000

    Add bodyparser for POST requests and set app PORT attribute

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 'use strict';
 
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
-app.set('PORT', 5000);
+const DEV_PORT = 5000;
+
+// configure
+
+app.set('PORT', DEV_PORT);
+
+// middlewares
+
+app.use(bodyParser().urlencoded({ extended: true }));
+
+// controllers
 
 app.get('/', function(req, res) { res.send('hello world') });
+
+// exports
 
 module.exports = app;
